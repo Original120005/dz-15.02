e014b39b22e326aca85ffbaf62b66a71c6d5166f
Дима <@DESKTOP-TC925PR>
2023-02-19 20:20:27 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace homework_15._02
+{
+    internal class Book : ICloneable, IComparable
+    {
+        private string name;
+        private string genre;
+        private int amount_of_pages;
+
+        public Book() { }
+        public Book(string name, string genre, int amount_)
+        {
+            this.name = name;
+            this.genre = genre;
+            this.amount_of_pages = amount_;
+        }
+
+        public object Clone()
+        {
+            return new Book(name, genre, amount_of_pages);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is Book)
+                return name.CompareTo((obj as Book).name);
+            throw new NotImplementedException();
+        }
+
+        public void Enter()
+        {
+            Console.Write("Enter name of book: ");
+            name = Console.ReadLine();
+            Console.Write("Enter genre book: ");
+            genre = Console.ReadLine();
+            Console.Write("Enter pages amount: ");
+            string aop = Console.ReadLine();
+            amount_of_pages = Convert.ToInt32(aop);
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Genre: {genre}");
+            Console.WriteLine($"Pages: {amount_of_pages}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_15._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] book = new Book[3];
+            book[0] = new Book("D", "J", 12);
+            book[1] = new Book("B", "J", 13);
+            book[2] = new Book("A", "J", 14);
+
+            foreach (Book b in book)
+            {
+                b.Print();
+            }
+
+            Array.Sort(book);
+            Console.WriteLine("New massive:");
+            foreach (Book b in book)
+            {
+                b.Print();
+            }
+
+
+        }
+    }
+}
